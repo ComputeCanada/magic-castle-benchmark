e194e035a3cf6b178a6d0d91d563e3ff04714bc5
Etienne <etienne.dubeau@calculquebec.ca>
2024-03-14 20:57:16 -0400

    Update

--- demo-infra/main.tf
+++ demo-infra/main.tf
@@ -36,6 +36,8 @@ module "openstack" {
     }
   }
 
+  hieradata = "---"
+
   public_keys = ["ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIL7dd00/9CXlTohQEgj5scMu1gOqrixPDVxF6Hrh67sD etiennedub"]
 
   nb_users = 10
