8d0733f8d82a2ccc630657ea9ad6da53939cb490
Etienne <etienne.dubeau@calculquebec.ca>
2024-05-01 12:48:56 -0400

    Update dashboard

--- dashboard/main.py
+++ dashboard/main.py
@@ -29,6 +29,38 @@ def connect_to_opensearch(username, password, host, port):
         st.error("Error connecting to OpenSearch. Please check your credentials and try again.")
         return None
 
+def get_workspace_from_run_id(es, index, run_id):
+    try:
+        body = {
+          "size": 0,
+          "query": {
+            "term": {
+              "run_id": run_id
+            }
+          },
+          "aggs": {
+            "unique_workspaces": {
+              "terms": {
+                "field": "workspace",
+                "size": 10
+              }
+            }
+          }
+        }
+
+        res = es.search(index=f"{index}", body=body)
+        buckets = res['aggregations']['unique_workspaces']['buckets']
+        if len(buckets) != 1:
+            raise Exception("Unknown workspace")
+        else:
+            return buckets[0]['key']
+
+    except Exception as e:
+        logger.error(f"Error searching workspace for {run_id=}: {e}")
+        st.error(f"Error searching workspace for {run_id=}. Please try again.")
+        return pd.DataFrame()
+
+
 def search_start_end(es, index, run_id, program):
     try:
         body = {
@@ -86,6 +118,7 @@ def search_start_end(es, index, run_id, program):
         aggregations = res['aggregations']
         missing_host = aggregations['missing_host']
         if missing_host['doc_count'] != 0:
+            # In Terraform case, there is no host
             start = pd.to_datetime(missing_host['min_timestamp']['value_as_string'])
             end = pd.to_datetime(missing_host['max_timestamp']['value_as_string'])
             host = None
@@ -102,7 +135,7 @@ def search_start_end(es, index, run_id, program):
 
     except Exception as e:
         logger.error(f"Error searching {program} logs: {e}")
-        st.error(f"Error searching {program} logs. Please try again.")
+        st.error(f"No {program} logs for {run_id}.")
         return pd.DataFrame()
 
 def search_puppet(es, index, run_id):
@@ -162,12 +195,12 @@ def search_puppet(es, index, run_id):
         return df
     except Exception as e:
         logger.error(f"Error searching puppet logs: {e}")
-        st.error("Error searching puppet logs. Please try again.")
+        st.error(f"No puppet logs for {run_id}.")
         return pd.DataFrame()
 
 def list_unique_values(es, index, key):
     try:
-        query = {"size": 0, "aggs": {"unique_values": {"terms": {"field": key}}}}
+        query = {"size": 0, "aggs": {"unique_values": {"terms": {"field": key, "size": 1000}}}}
         res = es.search(index=index, body=query)
         unique_values = [bucket["key"] for bucket in res["aggregations"]["unique_values"]["buckets"]]
         return unique_values
@@ -184,19 +217,21 @@ def get_single_run(es, index, run_id):
     puppet_df = search_puppet(es, INDEX, run_id)
     puppet_df['program'] = "puppet"
 
+    workspace = get_workspace_from_run_id(es, index, run_id)
+
     df = pd.concat([terraform_df, puppet_df, cloudinit_df], ignore_index=True)
     df['run_id'] = run_id
+    df['workspace'] = workspace
     return df
 
-def get_all_run(es, index, run_ids):
+@st.cache_data
+def get_all_run(_es, index, run_ids):
     dfs = []
     for run_id in run_ids:
-        dfs.append(get_single_run(es, index, run_id))
+        dfs.append(get_single_run(_es, index, run_id))
     df = pd.concat(dfs)
 
     df['duration_s'] = (df['end'] - df['start']).dt.total_seconds()
-    # total_duration = df.groupby('run_id')['end'].max() -  df.groupby('run_id')['start'].min()
-    # breakpoint()
     return df
 
 def main():
@@ -218,16 +253,44 @@ def main():
     if es:
         run_ids = list_unique_values(es, INDEX, "run_id")
         df = get_all_run(es, INDEX, run_ids)
-        program_duration = df.groupby(['run_id', 'program'])['duration_s'].max().reset_index()
-        fig = px.bar(program_duration, x='run_id', y='duration_s', color='program', barmode='stack')
+
+        workspaces = list_unique_values(es, INDEX, "workspace")
+        workspaces_options = st.multiselect(
+            'Clouds', workspaces, default=workspaces, format_func=lambda x : x.title())
+        df = df[df['workspace'].isin(workspaces_options)]
+
+        min_date = df['start'].min().to_pydatetime()
+        max_date = df['end'].max().to_pydatetime()
+        date_range = st.slider(
+            "Date range",
+            value=(min_date, max_date),
+            min_value=min_date, max_value=max_date)
+        df = df[(df['start'] >= date_range[0]) & (df['end'] <= date_range[1])]
+
+
+        program_duration = df.groupby(['run_id', 'program', 'workspace'])['duration_s'].max().reset_index()
+        run_start = df.groupby(['run_id', 'workspace'])['start'].min().reset_index()
+        result = pd.merge(program_duration, run_start, on=['run_id', 'workspace'])
+
+        fig = px.bar(result, x='start', y='duration_s', color='program',
+            barmode='stack', facet_col='workspace',
+            category_orders={'program': ['terraform', 'cloudinit', 'puppet']},
+            labels={
+                "start": "Date",
+                "duration_s": "Run duration (s)",
+                "program": "Program"
+             },)
+        fig.for_each_annotation(lambda a: a.update(text=a.text.split("=")[-1].title()))
         st.plotly_chart(fig)
 
-        run_id = st.selectbox("Run IDs", run_ids, index=None)
+        run_id = st.selectbox("Run IDs", df['run_id'].unique(), index=None)
         if run_id:
             df_single = df[df['run_id'] == run_id]
 
             if not df_single.empty:
-                fig = px.timeline(df_single, x_start="start", x_end="end", y="host", color="program")
+                fig = px.timeline(df_single, x_start="start", x_end="end", y="host", color="program",
+                     category_orders={'program': ['terraform', 'cloudinit', 'puppet']})
+
                 fig.update_yaxes(autorange="reversed")
                 st.plotly_chart(fig)
 
