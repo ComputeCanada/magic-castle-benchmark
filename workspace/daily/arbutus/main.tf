4a3ddd04a55408e2013df0f01209a11cf8890c19
Etienne <etienne.dubeau@calculquebec.ca>
2024-07-17 08:53:51 -0400

    Update to upstream MC puppet repo

--- workspace/daily/arbutus/main.tf
+++ workspace/daily/arbutus/main.tf
@@ -4,28 +4,28 @@ terraform {
 
 module "openstack" {
   source         = "git::https://github.com/ComputeCanada/magic_castle.git//openstack"
-  config_git_url = "https://github.com/etiennedub/puppet-magic_castle.git"
-  config_version = "vector"
+  config_git_url = "https://github.com/ComputeCanada/puppet-magic_castle.git"
+  config_version = "main"
 
   cluster_name = "mcspeed"
   domain       = "calculquebec.cloud"
   image        = "Rocky-8"
 
   instances = {
-    mgmt   = { type = "p4-6gb", tags = ["puppet", "mgmt", "nfs"], count = 1 }
-    login  = { type = "p4-6gb", tags = ["login", "public", "proxy"], count = 1 }
-    node   = { type = "p2-3gb", tags = ["node"], count = 1 }
+    mgmt  = { type = "p4-6gb", tags = ["puppet", "mgmt", "nfs"], count = 1 }
+    login = { type = "p4-6gb", tags = ["login", "public", "proxy"], count = 1 }
+    node  = { type = "p2-3gb", tags = ["node"], count = 1 }
   }
 
   volumes = {
     nfs = {
-      home     = { size = 10 }
-      project  = { size = 5 }
-      scratch  = { size = 5 }
+      home    = { size = 10 }
+      project = { size = 5 }
+      scratch = { size = 5 }
     }
   }
 
-  public_keys = ["ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIL7dd00/9CXlTohQEgj5scMu1gOqrixPDVxF6Hrh67sD mcspeed"]
+  public_keys      = ["ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIL7dd00/9CXlTohQEgj5scMu1gOqrixPDVxF6Hrh67sD mcspeed"]
   generate_ssh_key = true
 
   hieradata = file("./hieradata.yaml")
--- workspace/weekly/aws/main.tf
+++ workspace/weekly/aws/main.tf
@@ -5,38 +5,38 @@ terraform {
 module "aws" {
 
   source         = "git::https://github.com/ComputeCanada/magic_castle.git//aws"
-  config_git_url = "https://github.com/etiennedub/puppet-magic_castle.git"
-  config_version = "vector"
+  config_git_url = "https://github.com/ComputeCanada/puppet-magic_castle.git"
+  config_version = "main"
 
   cluster_name = "mcspeed"
   domain       = "calculquebec.cloud"
   image        = "ami-09ada793eea1559e6"
 
   instances = {
-    mgmt  = { type = "t3.large",  count = 1, tags = ["mgmt", "puppet", "nfs"] },
+    mgmt  = { type = "t3.large", count = 1, tags = ["mgmt", "puppet", "nfs"] },
     login = { type = "t3.medium", count = 1, tags = ["login", "public", "proxy"] },
     node  = { type = "t3.medium", count = 1, tags = ["node"] }
   }
 
   volumes = {
     nfs = {
-      home     = { size = 10, type = "gp2" }
-      project  = { size = 50, type = "gp2" }
-      scratch  = { size = 50, type = "gp2" }
+      home    = { size = 10, type = "gp2" }
+      project = { size = 50, type = "gp2" }
+      scratch = { size = 50, type = "gp2" }
     }
   }
 
-  public_keys = ["ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIL7dd00/9CXlTohQEgj5scMu1gOqrixPDVxF6Hrh67sD mcspeed"]
+  public_keys      = ["ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIL7dd00/9CXlTohQEgj5scMu1gOqrixPDVxF6Hrh67sD mcspeed"]
   generate_ssh_key = true
 
-  nb_users     = 10
+  nb_users = 10
   # Shared password, randomly chosen if blank
   guest_passwd = ""
 
   hieradata = file("./hieradata.yaml")
 
   # AWS specifics
-  region            = "ca-central-1"
+  region = "ca-central-1"
 }
 
 output "accounts" {
