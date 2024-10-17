723625fa6dc5b5eef1f02cb26cf606adf014e5e9
etienned <etienne.dubeau@calculquebec.ca>
2024-10-17 13:59:06 -0400

    Update to upstream repo

--- workspace/daily/beluga/main.tf
+++ workspace/daily/beluga/main.tf
@@ -4,8 +4,8 @@ terraform {
 
 module "openstack" {
   source         = "git::https://github.com/ComputeCanada/magic_castle.git//openstack"
-  config_git_url = "https://github.com/etiennedub/puppet-magic_castle.git"
-  config_version = "datahog_key"
+  config_git_url = "https://github.com/ComputeCanada/puppet-magic_castle.git"
+  config_version = "main"
 
   cluster_name = "mcspeed"
   domain       = "calculquebec.cloud"
